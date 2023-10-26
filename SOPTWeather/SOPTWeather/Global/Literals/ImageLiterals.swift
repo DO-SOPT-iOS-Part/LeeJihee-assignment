4f2b75b6dbeda274439ce80dd208950d6aabea32
Zoe <mam07065@naver.com>
2023-10-27 06:28:27 +0900

    Fix/#1 imageLiteral 수정

--- SOPTWeather/SOPTWeather/Global/Literals/ImageLiterals.swift
+++ SOPTWeather/SOPTWeather/Global/Literals/ImageLiterals.swift
@@ -8,29 +8,32 @@
 import UIKit
 
 class ImageLiterals {
-    static var backgroundFull = UIImage(named: "background-full")
+    //background
+    static var backgroundFull = UIImage(named: "background-full")!
     
-    static var backgroundList = UIImage(named: "background-list")
+    static var backgroundList = UIImage(named: "background-list")!
     
-    static var mapIcon = UIImage(systemName: "map.fill")?.withTintColor(.white)
+    //buttonIcon
+    static var mapIcon = UIImage(systemName: "map.fill")!.withTintColor(.white)
     
-    static var listIcon = UIImage(systemName: "list.bullet")?.withTintColor(.white)
+    static var listIcon = UIImage(systemName: "list.bullet")!.withTintColor(.white)
     
-    static var locationIcon = UIImage(systemName: "location.fill")?.withTintColor(.white)
+    static var locationIcon = UIImage(systemName: "location.fill")!.withTintColor(.white)
     
-    static var ellipsisIcon = UIImage(systemName: "ellipsis.circle")?.withTintColor(.white)
+    static var ellipsisIcon = UIImage(systemName: "ellipsis.circle")!.withTintColor(.white)
     
-    static var sunIcon = UIImage(systemName: "sun.max.fill")?.withTintColor(.white)
+    // weatherIcon
+    static var sunIcon = UIImage(systemName: "sun.max.fill")!.withTintColor(.white)
     
-    static var drizzleRainIcon = UIImage(systemName: "cloud.drizzle.fill")?.withTintColor(.white)
+    static var drizzleRainIcon = UIImage(systemName: "cloud.drizzle.fill")!.withTintColor(.white)
     
-    static var heavyRainIcon = UIImage(systemName: "cloud.heavyrain.fill")?.withTintColor(.white)
+    static var heavyRainIcon = UIImage(systemName: "cloud.heavyrain.fill")!.withTintColor(.white)
     
-    static var cloudBoltIcon = UIImage(systemName: "cloud.bolt.fill")?.withTintColor(.white)
+    static var cloudBoltIcon = UIImage(systemName: "cloud.bolt.fill")!.withTintColor(.white)
     
-    static var cloudSunRainIcon = UIImage(systemName: "cloud.sun.rain.fill")?.withTintColor(.white)
+    static var cloudSunRainIcon = UIImage(systemName: "cloud.sun.rain.fill")!.withTintColor(.white)
     
-    static var cloudMoonIcon = UIImage(systemName: "cloud.moon.fill")?.withTintColor(.white)
+    static var cloudMoonIcon = UIImage(systemName: "cloud.moon.fill")!.withTintColor(.white)
     
     //cloud.sun.rain.fill
 }
