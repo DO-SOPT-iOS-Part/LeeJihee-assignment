887cb49182142e43a698afc62175196b9fb75190
Zoe <mam07065@naver.com>
2023-10-27 06:27:58 +0900

    Fix/#1 FontError Fix

--- SOPTWeather/SOPTWeather/Global/Resource/Font.swift
+++ SOPTWeather/SOPTWeather/Global/Resource/Font.swift
@@ -11,59 +11,59 @@ extension UIFont {
     
     // SF-Pro-Bold
     class var sfProBold36: UIFont {
-        return UIFont(name: "SF-Pro-Display-Bold", size: 36)!
+        return UIFont(name: "SFProDisplay-Bold", size: 36)!
     }
     
     class var sfProBold24: UIFont {
-        return UIFont(name: "SF-Pro-Display-Bold", size: 24)!
+        return UIFont(name: "SFProDisplay-Bold", size: 24)!
     }
     
     // SF-Pro-Medium
     class var sfProMedium22: UIFont {
-        return UIFont(name: "SF-Pro-Display-Bold", size: 22)!
+        return UIFont(name: "SFProDisplay-Medium", size: 22)!
     }
     
     class var sfProMedium20: UIFont {
-        return UIFont(name: "SF-Pro-Display-Bold", size: 20)!
+        return UIFont(name: "SFProDisplay-Medium", size: 20)!
     }
     
     class var sfProMedium17: UIFont {
-        return UIFont(name: "SF-Pro-Display-Bold", size: 17)!
+        return UIFont(name: "SFProDisplay-Medium", size: 17)!
     }
     
     class var sfProMedium16: UIFont {
-        return UIFont(name: "SF-Pro-Display-Bold", size: 16)!
+        return UIFont(name: "SFProDisplay-Medium", size: 16)!
     }
     
     class var sfProMedium15: UIFont {
-        return UIFont(name: "SF-Pro-Display-Bold", size: 15)!
+        return UIFont(name: "SFProDisplay-Medium", size: 15)!
     }
     
     // SF-Pro-Regular
     class var sfProRegular36: UIFont {
-        return UIFont(name: "SF-Pro-Display-Bold", size: 36)!
+        return UIFont(name: "SFProDisplay-Regular", size: 36)!
     }
     
     class var sfProRegular24: UIFont {
-        return UIFont(name: "SF-Pro-Display-Bold", size: 24)!
+        return UIFont(name: "SFProDisplay-Regular", size: 24)!
     }
     
     class var sfProRegular19: UIFont {
-        return UIFont(name: "SF-Pro-Display-Bold", size: 19)!
+        return UIFont(name: "SFProDisplay-Regular", size: 19)!
     }
     
     class var sfProRegular18: UIFont {
-        return UIFont(name: "SF-Pro-Display-Bold", size: 18)!
+        return UIFont(name: "SFProDisplay-Regular", size: 18)!
     }
     
     // SF-Pro-Light
     class var sfProLight52: UIFont {
-        return UIFont(name: "SF-Pro-Display-Bold", size: 52)!
+        return UIFont(name: "SFProDisplay-Light", size: 52)!
     }
     
     // SF-Pro-Thin
-    class var sfProThin: UIFont {
-        return UIFont(name: "SF-Pro-Display-Bold", size: 102)!
+    class var sfProThin102: UIFont {
+        return UIFont(name: "SFProDisplay-Thin", size: 102)!
     }
     
 }
