b23d6f886a33aa856ae01e6f44765ec059827b57
Zoe <Zoe.urssu@gmail.com>
2023-11-10 18:53:46 +0900

    Feat/#6 셀 레이아웃 잡기

--- SOPTWeather/SOPTWeather/Presentation/WeatherDetail/Cells/WeeklyWeatherCollectionViewCell.swift
+++ SOPTWeather/SOPTWeather/Presentation/WeatherDetail/Cells/WeeklyWeatherCollectionViewCell.swift
@@ -9,4 +9,98 @@ import UIKit
 
 class WeeklyWeatherCollectionViewCell: UICollectionViewCell {
     
+    
+    private let weekLabel: UILabel = {
+        let label = UILabel()
+        label.font = .sfProMedium22
+        label.textColor = .white
+        return label
+    }()
+    
+    private let WeathericonImageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    private let precipitationLabel: UILabel = {
+        let label = UILabel()
+        label.font = .sfProMedium15
+        label.textColor = .blue
+        return label
+    }()
+    
+    private lazy var weatherStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.addArrangeSubViews(WeathericonImageView, precipitationLabel)
+        return stackView
+    }()
+    
+    private let minTempLabel: UILabel = {
+        let label = UILabel()
+        label.font = .sfProMedium22
+        label.textColor = .white.withAlphaComponent(0.3)
+        return label
+    }()
+    
+    private let gradientView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    private let maxTempLabel: UILabel = {
+        let label = UILabel()
+        label.font = .sfProMedium22
+        label.textColor = .white
+        return label
+    }()
+    
+    private lazy var horizontalStackView : UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .equalCentering
+        stackView.addArrangeSubViews(weekLabel,weatherStackView,minTempLabel,gradientView,maxTempLabel)
+        stackView.setCustomSpacing(10, after: minTempLabel)
+        stackView.setCustomSpacing(10, after: gradientView)
+        return stackView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setData(week:String, icon: UIImage, min:String, gradient: UIImage, max: String) {
+        weekLabel.text = week
+        WeathericonImageView.image = icon
+        minTempLabel.text = min
+        gradientView.image = gradient
+        maxTempLabel.text = max
+    }
+    
+    private func setUI(){
+        setViewHierarchy()
+        setConstraints()
+    }
+    
+    private func setViewHierarchy() {
+        self.addSubViews(horizontalStackView)
+    }
+    
+    private func setConstraints() {
+        horizontalStackView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    
+}
+
+extension WeeklyWeatherCollectionViewCell: CollectionViewCellReuseProtocol {
+    
 }
